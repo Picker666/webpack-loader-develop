5eb44ea96c8407db9d727abc659bf652857c906d
Picker666 <1034790395@qq.com>
2023-05-08 09:44:52 +0800

    a loader to sprite image

--- run-loader.js
+++ run-loader.js
@@ -3,17 +3,20 @@ const fs = require('fs')
 const path = require('path')
 
 runLoaders({
-  resource: path.join(__dirname, './src/demo.txt'),
+  resource: path.join(__dirname, './src/index.css'),
   loaders: [
+    // {
+    //   loader: path.join(__dirname, './src/raw-loader.js'),
+    //   // 给loader传递参数
+    //   options: {
+    //     name: 'raw-loader...'
+    //   }
+    // },
+    // {
+    //   loader: path.join(__dirname, './src/async-loader.js')
+    // }
     {
-      loader: path.join(__dirname, './src/raw-loader.js'),
-      // 给loader传递参数
-      options: {
-        name: 'raw-loader...'
-      }
-    },
-    {
-      loader: path.join(__dirname, './src/async-loader.js')
+        loader: path.join(__dirname, './src/sprite-loader.js')
     }
   ],
   context: {
